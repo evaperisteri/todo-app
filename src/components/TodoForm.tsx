5efebec0e1dc257dac433e1e72bda02432fa4df5
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-05-29 19:47:51 +0300

    initial commit

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+type Action =
+    | {type: "ADD"; payload: string}
+    | {type: "DELETE"; payload: string};
+
+
+type TodoFormProps = {
+    dispatch: React.Dispatch<Action>;
+}
+const TodoForm =({dispatch}: TodoFormProps)=>{
+
+    const [text, setText] = useState("");
+    const handleChange= (e: React.ChangeEvent<HTMLInputElement>)=>{
+        setText(e.target.value);
+    };
+
+    const handleSubmit= (e: React.FormEvent)=>{
+        e.preventDefault();
+        if(text.trim() !== "")
+            dispatch({type: "ADD", payload: text});
+        setText("");
+    }
+    return(
+        <>
+            <form onSubmit={handleSubmit} className="flex gap-4 mb-4">
+                <input type="text" value={text} onChange={handleChange} className="border p-2 rounded" placeholder="New task..."/>
+                <button type="submit" className="bg-cf-dark-gray text-white px-4 py-2 rounded"> Add </button>
+            </form>
+        </>
+    )
+};
+export default TodoForm;
