8301adcf270756bd995f934a9ab66dcca1891d1e
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-06-05 21:55:36 +0300

    feat: clear all tasks and count tasks

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -24,6 +24,8 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
             return state.map(todo => todo.id===action.payload.id?{...todo, text:action.payload.newText}:todo);
         case "COMPLETE":
             return state.map(todo => todo.id===action.payload?{...todo, completed: !todo.completed}:todo);
+        case "CLEAR_ALL":
+            return [];
         default:
             return state;
     }
@@ -31,9 +33,17 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
 
 const Todo = () =>{
     const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos);
+    const totalTasks:number = todos.length;
+    const completedTasks:number = todos.filter(t=> t.completed).length;
+    const activeTasks:number = totalTasks-completedTasks;
+
     useEffect(()=>{
        localStorage.setItem("todos", JSON.stringify(todos))
     },[todos])
+
+    const handleClearAll = ()=>{
+        dispatch({type: "CLEAR_ALL"})
+    }
 
     return (
         <>
@@ -41,6 +51,18 @@ const Todo = () =>{
                 <h1 className="text-center text-2xl mb-4">To-Do List</h1>
                 <TodoForm dispatch={dispatch} />
                 <TodoList todos={todos} dispatch={dispatch} />
+                { todos.length > 0 && (
+                    <>
+                        <div className="flex justify-between mt-4 border-t pt-2 text-cf-gray">
+                            <span>Total: {totalTasks}</span>
+                            <span>Active: {activeTasks}</span>
+                            <span>Completed: {completedTasks}</span>
+                        </div>
+                        <div className="text-end mt-4">
+                            <button onClick={handleClearAll} className="bg-cf-dark-red text-white py-2 px-4 rounded">Clear All</button>
+                        </div>
+                    </>
+                )}
             </div>
         </>
     )
